522a7331b9d49aa4d234e613ac74522458cdd168
Jim <jim@tqis.com>
2015-10-01 14:21:58 -0400

    Stubbed out RomanNumbers service.
    There is probably an official technique for accessing a services
    internals for unit testing.

--- app/scripts/services/romannumbers.js
+++ app/scripts/services/romannumbers.js
@@ -10,4 +10,18 @@
 angular.module('kataApp')
   .service('RomanNumbers', function () {
     // AngularJS will instantiate a singleton by calling "new" on this function
+  var _this = {};
+  var expose = {};
+
+  _this.expose = function () { return expose; };
+
+  _this.toRoman = function ( number ) {
+    return number.toString();
+  } ;
+
+  _this.fromRoman = function ( string ) {
+    parseInt( string );
+  } ;
+
+  return _this;
   });
--- test/spec/services/romannumbers.js
+++ test/spec/services/romannumbers.js
@@ -1,5 +1,16 @@
 'use strict';
 
+/**
+ * Roman Numerals are string representation of numbers.  The API might
+ * best create methods to JavaScript Number and String built-in classes,
+ * as the toRoman() method is functionally identical to Number.toString() 
+ * except the resulting representation is uniquely Roman.  Only cardinal 
+ * numbers can be converted.
+ *
+ * The internals of the RomanNumbers service need to be exposed for testing
+ * using the public method expose().
+ */
+
 describe('Service: RomanNumbers', function () {
 
   // load the service's module
